1169bf6d87b10eeeb43f2033559565f2662e1743
UvinduV <uvindusanketh470@gmail.com>
2025-02-19 10:13:03 +0530

    create crop model

---
+++ model/Crop.ts
@@ -0,0 +1,9 @@
+export class Crop{
+    cropCode!:string;
+    commonName!:string;
+    scientificName!:string;
+    cropImage1!:string;
+    category!:string;
+    season!:string;
+    fieldId!:string;
+}
