29574541af926ea11c5ca39d07b48ed139959932
UvinduV <uvindusanketh470@gmail.com>
2025-02-19 23:03:45 +0530

    create vehicle model

---
+++ model/Vehicle.ts
@@ -0,0 +1,9 @@
+export class Vehicle{
+    vehicleCode!:number;
+    licensePlateNumber!:string;
+    vehicleCategory!:string;
+    fuelType!:string;
+    status!:string;
+    remarks!:string;
+    staffId!:number;
+}
