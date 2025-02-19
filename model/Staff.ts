1848248e1b4f6520af1c59d31cbd8b90e1c0e137
UvinduV <uvindusanketh470@gmail.com>
2025-02-19 11:40:35 +0530

    create staff model

---
+++ model/Staff.ts
@@ -0,0 +1,14 @@
+export class Staff{
+    staffId!:number;
+    firstName!:string;
+    lastName!:string;
+    designation!:string;
+    gender!:string;
+    joinedDate!:string;
+    dob!:string;
+    address!:string;
+    contactNo!:string;
+    email!:string;
+    role!:string;
+    fieldId!:number;
+}
